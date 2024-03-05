26ae017ec3c5ed4f35d9804292ba4f9299b174de
Camilo <ocblanco03@ucatolica.edu.co>
2024-03-05 00:48:55 -0500

    add isp

---
+++ src/main/java/com/mycompany/principiossolid/isp/SMSSender.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.principiossolid.isp;
+
+/**
+ *
+ * @author camilo
+ */
+public class SMSSender {
+
+    public static void sendSMS(ContactInformation c, String message) {
+         String phoneNumber = c.getTelephone();
+       //Envía un mensaje SMS al teléfono del Contacto  c.  
+    }
+}
